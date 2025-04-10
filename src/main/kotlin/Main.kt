f93f1d46f81239467d040734b65c49c71f39404b
tommyemk <159800790+tommyemk@users.noreply.github.com>
2025-04-10 13:31:20 +0200

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import view.GameApplication
+
+/**
+ * This method is starting the game.
+ */
+fun main() {
+    GameApplication().show()
+    //SopraApplication().show()
+    println("Application ended. Goodbye")
+}
